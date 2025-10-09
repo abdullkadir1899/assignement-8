20cb74f6945e205421dc863d588bdfa3f911bcdd
abdullkadir1899 <mdabdulkadir1899@gmail.com>
2025-10-09 06:35:43 +0600

    remved the section

--- src/pages/Installation/addToDB.js
+++ src/pages/Installation/addToDB.js
@@ -1,10 +1,8 @@
 // addToDB.js
-
-// অ্যাপ ডেটা লোকাল স্টোরেজ থেকে নিয়ে আসে
 const getStoredApps = () => {
-    const storedAppSTR = localStorage.getItem('appsList');
+    const storedAppSTR = localStorage.getItem('appsList')
 
-    if(storedAppSTR) {
+    if(storedAppSTR){
         const storedAppsData = JSON.parse(storedAppSTR)
         return storedAppsData
     }
@@ -13,53 +11,40 @@ const getStoredApps = () => {
     }
 }
 
-// অ্যাপ আইডি স্টোরেজে যোগ করে
-const addToStoredDB = (id) => {
-
+// ad 
+const addToStoredDB = (id)  => {
     const storedAppsData = getStoredApps();
-    
-    if(storedAppsData.includes(id.toString())) { // ID স্ট্রিং হিসেবে চেক করা হলো
-        return 'already_added'; // স্ট্যাটাস রিটার্ন
+
+    if(storedAppsData.includes(id.toString())){
+        return 'Already_added'
     }
     else{
-        storedAppsData.push(id.toString()) // ID স্ট্রিং হিসেবে সেভ করা
-        const data = JSON.stringify(storedAppsData);
+        storedAppsData.push(id.toString())
+        const data = JSON.stringify(storedAppsData)
         localStorage.setItem('appsList', data)
-        return 'added_successfully'; // স্ট্যাটাস রিটার্ন
+        return 'added_successfully'
     }
 }
 
 
-// অ্যাপ আইডি রিমুভ করে
+
+//remove
 const removeFromStoredDB = (id) => {
     const storedAppsData = getStoredApps();
 
-    const idToRemove = id.toString(); 
-    const updatedData = storedAppsData.filter(appId => appId !== idToRemove);
+    const idToRemove = id.toString();
+    const updateData = storedAppsData.filter(appId => appId !== idToRemove);
 
-    if (updatedData.length === storedAppsData.length) {
-        return 'not_found'; // স্ট্যাটাস রিটার্ন
-    } else {
-        localStorage.setItem('appsList', JSON.stringify(updatedData));
-        console.log('Removed:', idToRemove);
-        return 'removed_successfully'; // স্ট্যাটাস রিটার্ন
+    if(updateData.length === storedAppsData.length){
+        return 'not_found'
     }
-};
+    else{
+        localStorage.setItem('appsList', JSON.stringify(updateData));
+        console.log('Removed', idToRemove)
+        return 'removed_successfully'
+    }
+
+}
 
 
 export { addToStoredDB, getStoredApps, removeFromStoredDB }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
